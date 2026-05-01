30ca5d18896450de383451bfe3f3269851ab66d2
omar52344 <omarjaramillo8@gmail.com>
2026-04-30 23:06:15 -0500

    Add data sink module with file and database support

---
+++ src/storage/sink/mod.rs
@@ -0,0 +1,22 @@
+#[cfg(feature = "db")]
+pub mod db;
+pub mod file;
+
+use crate::error::Result;
+use crate::model::streaming::StreamingDocument;
+use async_trait::async_trait;
+use serde::Serialize;
+use serde_json::Value;
+
+#[derive(Serialize)]
+#[serde(untagged)]
+pub enum SinkItem {
+    Document(StreamingDocument),
+    Raw(Value),
+}
+
+#[async_trait]
+pub trait DataSink: Send + Sync {
+    async fn send(&mut self, item: SinkItem) -> Result<()>;
+    async fn flush(&mut self) -> Result<()>;
+}
